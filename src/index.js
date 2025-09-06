6fe4f3c5c4e3dd1b537f2a5d860b7f7339246482
Borys Ziukov <ziukoff1985@gmail.com>
2025-09-06 18:42:20 +0300

    Add method flat in prototype with recursion function

--- src/index.js
+++ src/index.js
@@ -10,7 +10,27 @@ function MyArray(...args) {
 const flattenArr = new MyArray(
     1,
     2,
-    new MyArray(1, 2, new MyArray(1, 2)),
-    new MyArray(1, 2)
+    new MyArray(10, 20, new MyArray(100, 200, new MyArray(1000, 2000))),
+    new MyArray(10, 20)
 );
 console.log(flattenArr);
+
+MyArray.prototype.flat = function (depth = 1) {
+    const result = new MyArray();
+
+    function flatten(source, currentDepth) {
+        for (let i = 0; i < source.length; i++) {
+            if (source[i] instanceof MyArray && currentDepth > 0) {
+                flatten(source[i], currentDepth - 1);
+            } else {
+                result[result.length] = source[i];
+                result.length++;
+            }
+        }
+    }
+
+    flatten(this, depth);
+    return result;
+};
+
+console.log(flattenArr.flat(Infinity));
