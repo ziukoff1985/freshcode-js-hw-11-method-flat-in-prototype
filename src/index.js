6e9bd961879e66cce7d49eb2c55ef7df35094225
Borys Ziukov <ziukoff1985@gmail.com>
2025-09-06 17:19:21 +0300

    Add constructor

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
 'use strict';
+
+function MyArray(...args) {
+    this.length = args.length;
+    for (let i = 0; i < args.length; i++) {
+        this[i] = args[i];
+    }
+}
+
+const flattenArr = new MyArray(
+    1,
+    2,
+    new MyArray(1, 2, new MyArray(1, 2)),
+    new MyArray(1, 2)
+);
+console.log(flattenArr);
